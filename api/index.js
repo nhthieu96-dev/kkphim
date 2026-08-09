264610446a4443680eb4e23b97691a1afdc1cc1a
nhthieu96-dev <trunghieu190496@gmail.com>
2026-08-09 12:27:40 +0700

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
+// Entry point cho Vercel (serverless function). Xem lib/addon-builder.js cho
+// toàn bộ logic addon.
 
+const { getRouter } = require("stremio-addon-sdk");
+const builder = require("../lib/addon-builder");
+
+const router = getRouter(builder.getInterface());
+
+module.exports = (req, res) => {
+  router(req, res, () => {
+    res.statusCode = 404;
+    res.end("Not found");
+  });
+};
